0869ada729aeee9fb5451f67a89e36f5a01df79a
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-14 14:26:01 +0200

    main with button

--- core/src/main/java/fr/supdevinci/game_proj/Main.java
+++ core/src/main/java/fr/supdevinci/game_proj/Main.java
@@ -6,27 +6,68 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture cardSprite;
+    private Stage stage;
+    private TextButton button;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("Cards.png");
+        cardSprite = new Texture("Cards.png");
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        BitmapFont font = new BitmapFont();
+        TextButtonStyle buttonStyle = new TextButtonStyle();
+        buttonStyle.font = font;
+        buttonStyle.fontColor = Color.BLACK;
+
+        button = new TextButton("Jouer", buttonStyle);
+        button.setSize(200, 50);
+        button.setPosition(
+            (Gdx.graphics.getWidth() - button.getWidth()) / 2,
+            (Gdx.graphics.getHeight() - button.getHeight()) / 2
+        );
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Bouton cliqué !");
+            }
+        });
+
+        stage.addActor(button);
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        cardSprite.dispose();
+        stage.dispose();
     }
 }
