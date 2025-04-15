eeb41e740e026373ae4b24efc35b54c645321e7a
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-15 14:15:26 +0200

    win logic

--- core/src/main/java/fr/supdevinci/game_proj/Logic.java
+++ core/src/main/java/fr/supdevinci/game_proj/Logic.java
@@ -1,6 +1,5 @@
 package fr.supdevinci.game_proj;
-// import java.util.ArrayList;
-// import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Logic {
 
@@ -23,5 +22,65 @@ public class Logic {
         }
 
         return "pass turn";
+    }
+
+    public static Integer roleDice() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    private Player compareSabacc(ArrayList<Player> sabacc) {
+        Player mainSabacc = null;
+        for (Player player : sabacc) {
+            if (mainSabacc == null) {
+                mainSabacc = player;
+                continue;
+            }
+            Integer mainSabaccValue = mainSabacc.getSandyCard().getValue();
+            Integer playerValue = player.getSandyCard().getValue();
+            if (mainSabaccValue > playerValue) {
+                mainSabacc = player;
+            }
+        }
+
+        return mainSabacc;
+    }
+
+    private Player compareWinOrder(ArrayList<Player> other) {
+        Player mainWinner = null;
+        for (Player player : other) {
+            if (mainWinner == null) {
+                mainWinner = player;
+                continue;
+            }
+            Integer mainWinnerValue = mainWinner.handValue();
+            Integer playerValue = player.handValue();
+            if (mainWinnerValue > playerValue) {
+                mainWinner = player;
+            }
+        }
+        return mainWinner;
+    }
+
+    public Player compareHands(ArrayList<Player> players) {
+        ArrayList<Player> sabacc = new ArrayList<>();
+        ArrayList<Player> winOrder = new ArrayList<>();
+
+        for (Player player : players) {
+            Integer handValue = player.handValue();
+            if (handValue == 0) {
+                sabacc.add(player);
+                continue;
+            }
+            else {
+                winOrder.add(player);
+            }
+        }
+
+        if (sabacc.size() != 0) {
+            return this.compareSabacc(sabacc);
+        }
+        else {
+            return this.compareWinOrder(winOrder);
+        }
     }
 }
