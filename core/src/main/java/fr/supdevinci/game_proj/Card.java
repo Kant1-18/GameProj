da4b75d3839cfe40a51c67c31e7eed122b36b2d9
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-15 11:11:29 +0200

    refacto Card et Round et add GameState

--- core/src/main/java/fr/supdevinci/game_proj/Card.java
+++ core/src/main/java/fr/supdevinci/game_proj/Card.java
@@ -8,9 +8,28 @@ public class Card {
 
     private static boolean assetLoaded = false;
 
-    public Card(Integer value, String color) {
+    public Integer getValue() { return value; }
+
+    public String getColor() { return color; }
+
+    public void setValue(Integer value)
+    {
+        if (value <= 0 || value >= 7) {
+            throw new IllegalArgumentException("Value must be between 0 and 7");
+        }
         this.value = value;
+    }
+
+    public void setColor(String color) {
+        if (color != "sandy" || color != "bloody") {
+            throw new IllegalArgumentException("Color must be either 'sandy' or 'bloody'");
+        }
         this.color = color;
+    }
+
+    public Card(Integer value, String color) {
+        this.setValue(value);
+        this.setColor(color);
 
         if (!assetLoaded) {
             CardAssets.load();
@@ -23,24 +42,5 @@ public class Card {
         if (color.equals("bloody")) {
             this.textureRegion = CardAssets.bloodyCards.get(value - 1);
         }
-    }
-
-    public Integer getValue() { return value; }
-
-    public void setValue(Integer value)
-    {
-        if (value <= 0 || value >= 7) {
-            throw new IllegalArgumentException("Value must be between 0 and 7");
-        }
-        this.value = value;
-    }
-
-    public String getColor() { return color; }
-
-    public void setColor(String color) {
-        if (color != "sandy" || color != "bloody") {
-            throw new IllegalArgumentException("Color must be either 'sandy' or 'bloody'");
-        }
-        this.color = color;
     }
 }
