731919e68ab01febbbd180a2df4aa4814edc2da4
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-04-22 11:39:23 +0200

    reparation

--- core/src/main/java/fr/supdevinci/game_proj/Deck.java
+++ core/src/main/java/fr/supdevinci/game_proj/Deck.java
@@ -3,26 +3,26 @@ package fr.supdevinci.game_proj;
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> cards;
+    private ArrayList<SabaccCard> cards;
 
-    public Deck(ArrayList<Card> cards) {
-        this.cards = new ArrayList<Card>(cards);
+    public Deck(ArrayList<SabaccCard> cards) {
+        this.cards = new ArrayList<SabaccCard>(cards);
     }
 
-    public ArrayList<Card> getCards() { return cards; }
+    public ArrayList<SabaccCard> getCards() { return cards; }
 
-    public Card pickCard() {
-        Card card = this.cards.get(0);
+    public SabaccCard pickCard() {
+        SabaccCard card = this.cards.get(0);
         this.cards.remove(0);
         return card;
     }
 
-    public void addCard(Card card) {
+    public void addCard(SabaccCard card) {
         this.cards.add(card);
     }
 
     public void shuffle() {
-        ArrayList<Card> shuffledCards = new ArrayList<>();
+        ArrayList<SabaccCard> shuffledCards = new ArrayList<>();
         while (this.cards.size() > 0) {
             int randomIndex = (int) (Math.random() * this.cards.size());
             shuffledCards.add(this.cards.get(randomIndex));
